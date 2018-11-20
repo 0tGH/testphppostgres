59d907535bb0ecbb9092c5a3bcf574ba21e0524e
0tGH <45216322+0tGH@users.noreply.github.com>
2018-11-21 08:28:58 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
-echo 'phpテスト<br>';
+echo 'test<br>';
 $dbinfo = parse_url(getenv('DATABASE_URL'));
 $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
